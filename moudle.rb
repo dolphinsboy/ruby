11f317456e3ae10bbd9a12cf1dffb4a35af42c41
dolphinsboy <sguo421@gmail.com>
2016-07-22 18:04:25 +0800

    添加module的demo

--- moudle.rb
+++ moudle.rb
@@ -5,4 +5,16 @@ def test
     p FileTest.size("/usr/bin/ruby")
 end
 
+module HelloModule
+    Version = "1.0"
+    def hello(name)
+        print "Hello, ", name, ".\n"
+    end
+
+    module_function :hello
+end
+
 test
+
+p HelloModule::Version
+HelloModule::hello("Guo")
