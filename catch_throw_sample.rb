259bc0f2fe858b39504df35fcb00ee0a68ec01c3
dolphinsboy <sguo421@gmail.com>
2016-07-26 11:54:41 +0800

    添加catch和throw对应的方法

---
+++ catch_throw_sample.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+def test_throw
+    throw :test
+end
+
+puts "test start"
+
+catch(:test){
+    puts "before test_throw()"
+    test_throw
+    puts "after test_throw()"
+}
+
+puts "test end"
